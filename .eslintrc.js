cb9dc09d03f39f267736bd6797ab52a525a9f488
Joe Soboleski <soboleskijoe@gmail.com>
2021-09-26 17:59:30 -0700

    vue init

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+  },
+
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint",
+  ],
+
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+  },
+
+  'extends': [
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
+  ]
+};
