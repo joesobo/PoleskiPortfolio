ec77f25624f5c525f8a39095c9e0806a46d7a380
Joe Soboleski <soboleskijoe@gmail.com>
2021-09-26 17:04:27 -0700

    background color

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+  <v-app :style="{ background: $vuetify.theme.themes[theme].background }">
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -38,19 +34,20 @@
     </v-app-bar>
 
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
+  name: "App",
+  computed: {
+    theme() {
+      return this.$vuetify.theme.dark ? "dark" : "light";
+    },
+  },
 });
 </script>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,16 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: "#161B22",
+        background: "#0E1117",
+      },
+    },
+  },
 });
