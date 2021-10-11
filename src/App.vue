abe49f4a3fe1ae95be46a101d39d8498d6e9023c
Joe <soboleskijoe@gmail.com>
2021-10-10 20:23:41 -0700

    refactor out css

--- src/App.vue
+++ src/App.vue
@@ -77,59 +77,8 @@ export default Vue.extend({
 
 <style>
 @import "styles/reset.css";
+@import "styles/base.css";
 @import url("https://fonts.googleapis.com/css?family=Open+Sans");
-
-body,
-html {
-  font-family: "Open Sans";
-}
-
-#app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: var(--text);
-  background: var(--background);
-  height: 100%;
-}
-
-.row {
-  display: flex;
-  flex-direction: row;
-}
-
-.col {
-  display: flex;
-  flex-direction: column;
-}
-
-.expand {
-  height: 100%;
-}
-
-.center {
-  justify-content: center;
-}
-
-.max {
-  width: 100%;
-  max-width: 1280px;
-  margin: 0 auto;
-}
-
-a {
-  color: var(--text);
-}
-
-h1 {
-  font-size: 2rem;
-  font-weight: 600;
-}
-
-h3 {
-  font-size: 1.25rem;
-  font-weight: 600;
-}
 </style>
 
 <style scoped>
